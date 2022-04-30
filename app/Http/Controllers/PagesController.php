c826dbcc04fbcc3166b081184d4490aeda36b037
LanaBallan <lanaballan9632@gmail.com>
2022-04-29 20:43:49 -0600

    Add views

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function courses()
+    {
+        return view('courses');
+    }
+    public function portfolio()
+    {
+        return view('portfolio');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
